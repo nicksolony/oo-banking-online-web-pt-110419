1e49a5daf56b30d242fcb498c22fef7896491f8b
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 16:20:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,6 @@
 class Transfer
+  
+  require "pry"
   attr_reader :sender, :receiver, :amount
   attr_accessor :status
   
@@ -14,6 +16,7 @@ class Transfer
   end
   
   def execute_transaction
+    binding.pry
     if sender.valid? && sender.balance >= @amount
       sender.balance -= @amount
       receiver.balance += @amount
