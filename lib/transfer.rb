854c0e9efb1bbb995c5ed744b63462b0533769ea
Nick Solonyy <nicksolony@gmail.com>
2019-12-25 16:23:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
   
   def execute_transaction
-    binding.pry
+ #   binding.pry
     if sender.valid? && sender.balance >= @amount
       sender.balance -= @amount
       receiver.balance += @amount
